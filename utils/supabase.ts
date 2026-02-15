0c6c752b3db55563c3c21e99dffaaf58b6973f3b
nevermature01 <nevermature01@gmail.com>
2026-02-15 05:43:43 +0000

    Update

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
@@ -8,4 +9,11 @@ if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase URL and Key must be provided.");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
